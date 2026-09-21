5812f4494ec0cd3f2b126d617d80a13d85b9b366
Jack wickham <Jack_wickham@icloud.com>
2026-09-21 12:30:58 +0100

    Add: buy-and-sell-crypto - submission-23

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-23.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = 0
+        sell = 1
+        maxProfit = 0
+        profit = 0
+        n = len(prices)
+
+        while buy<sell<n:
+            while prices[buy]>prices[sell] and sell <n-1:
+                buy = sell
+                sell+=1
+            
+            while sell<n-1 and prices[sell]<=prices[sell+1]:
+                sell+=1
+            
+            profit = prices[sell] - prices[buy]
+            if profit>maxProfit:
+                maxProfit = profit
+            sell+=1            
+            
+        return maxProfit
+            
+            
